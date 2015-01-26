e75f589671d4701102f1aca6c2894dc6c58f6852
LAU Thierry <thierlau@gmail.com>
2015-01-26 02:18:41 +0100

    editable directive

---
+++ app/js/directives/editable/editable.js
@@ -0,0 +1,40 @@
+angular.module('liste-invite-mariage').directive('editable', function ($timeout) {
+	"use strict";
+
+	return {
+		restrict: 'E',
+		templateUrl : 'js/directives/editable/editable.html',
+		replace: true,
+		scope: {
+			value : '=',
+			onSave : '&'
+		},
+		link : function (scope, element, attr) {
+
+			var save = function () {
+				scope.$apply(function () {
+					scope.editMode = false;
+					scope.onSave();
+					toast('Modification sauvegardée', 4000);
+				});
+			};
+
+			var input = element.find('input');
+
+			scope.editMode = false;
+
+			scope.enterEditMode = function () {
+				scope.editMode = true;
+				$timeout(function() {
+					$(input).focus();
+				});
+			};
+
+			input.on('keypress', function (event) {
+				if ( event.which == 13 ) {
+					save();
+				}
+			});
+		}
+	}
+});
