f35314e6da5f2ca334de21190607665ef57d6519
Thierry LAU <thierlau@gmail.com>
2015-02-08 18:40:45 +0100

    optimize cote filter

--- app/js/filters/matchCote.js
+++ app/js/filters/matchCote.js
@@ -3,24 +3,26 @@ angular.module('liste-invite-mariage').filter('matchCote', function () {
 
 	return function (list, showClaire, showThierry, showJeanPaul) {
 
-		return _.filter(list, function (invite) {
-			var criteria = false;
+		if (showClaire && showThierry && showJeanPaul ) {
+			return list;
+		} else {
+			return _.filter(list, function (invite) {
+				var criteria = false;
 
-			if (showClaire) {
-				criteria = criteria || invite.cote === 'Claire';
-			}
+				if (showClaire) {
+					criteria = criteria || invite.cote === 'Claire';
+				}
 
-			if (showThierry) {
-				criteria = criteria || invite.cote === 'Thierry';
-			}
+				if (showThierry) {
+					criteria = criteria || invite.cote === 'Thierry';
+				}
 
-			if (showJeanPaul) {
-				criteria = criteria || invite.cote === 'JP';
-			}
-			return criteria;
-		});
-
-
+				if (showJeanPaul) {
+					criteria = criteria || invite.cote === 'JP';
+				}
+				return criteria;
+			});
+		}
 	};
 
 });
